0d70d51e916c1d77fd00120dfad33dac2c6916e4
dhe05 <99608038+dhe05@users.noreply.github.com>
2022-04-19 23:13:19 +0800

    Add files via upload

--- He.Dan/parts/meta.php
+++ He.Dan/parts/meta.php
@@ -1,5 +1,5 @@
 	<meta name="viewport" content="width=device-width">
-
+	<base href="http://danhedesign.com/aau/wnm608/He.Dan/">
 	<link rel="stylesheet" type="text/css" href="lib/css/styleguide.css">
 	<link rel="stylesheet" type="text/css" href="lib/css/gridsystem.css">
 	<link rel="stylesheet" type="text/css" href="css/storetheme.css">
--- He.Dan/parts/navbar.php
+++ He.Dan/parts/navbar.php
@@ -1,8 +1,8 @@
-	
+<base href="http://danhedesign.com/aau/wnm608/He.Dan/">	
 	<header class="navbar">
 		<div class="container display-flex"> 
 			<div class="flex-none">
-				<h1>Store</h1>
+				<h1>eNest</h1>
 		</div>
 	
 		<div class="flex-stretch">
