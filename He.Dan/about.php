bb3c7cbd44ef23bbb89fefb583a435ab8f36a23c
dhe05 <99608038+dhe05@users.noreply.github.com>
2022-04-20 00:15:33 +0800

    Add files via upload

--- He.Dan/about.php
+++ He.Dan/about.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,4 +20,27 @@
 	 </div>
 	</div>
 </body>
+=======
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Product List</title>
+	<?php include "parts/meta.php"; ?>
+
+
+</head>
+<body>
+	<?php include "parts/navbar.php"; ?>
+
+	 <div class="container">
+	 	<div class="card soft">
+	 		<h2>About eNest</h2>
+	 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec diam nec ligula finibus auctor malesuada in ipsum. Aliquam cursus interdum fringilla. Suspendisse at sapien euismod mauris tristique commodo. Donec lorem felis, efficitur ultricies nulla sit amet, gravida hendrerit dui. Nullam porttitor tellus eu justo sollicitudin, nec mollis orci faucibus. Aliquam libero massa, fermentum at blandit at, blandit nec turpis. Nunc augue mi, laoreet egestas suscipit et, rutrum non purus. Quisque quis dui ante. Suspendisse ac pellentesque nisi. In hac habitasse platea dictumst.
+	 		</p>
+	 		
+	 </div>
+	</div>
+</body>
+>>>>>>> 0d70d51e916c1d77fd00120dfad33dac2c6916e4
 </html>
