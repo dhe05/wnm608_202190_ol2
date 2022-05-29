4b8477650a2e0dde8b3d25343ddfd57a77723324
dhe05 <99608038+dhe05@users.noreply.github.com>
2022-05-29 15:27:27 +0800

    Add files via upload

--- He.Dan/about.php
+++ He.Dan/about.php
@@ -1,46 +1,41 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Product List</title>
+	
 	<?php include "parts/meta.php"; ?>
-
 
 </head>
 <body>
-	<?php include "parts/navbar.php"; ?>
+<div class="view-window" style="background-image: url(img/gold.jpeg);">
 
-	 <div class="container">
-	 	<div class="card soft">
-	 		<h2>About eNest</h2>
-	 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec diam nec ligula finibus auctor malesuada in ipsum. Aliquam cursus interdum fringilla. Suspendisse at sapien euismod mauris tristique commodo. Donec lorem felis, efficitur ultricies nulla sit amet, gravida hendrerit dui. Nullam porttitor tellus eu justo sollicitudin, nec mollis orci faucibus. Aliquam libero massa, fermentum at blandit at, blandit nec turpis. Nunc augue mi, laoreet egestas suscipit et, rutrum non purus. Quisque quis dui ante. Suspendisse ac pellentesque nisi. In hac habitasse platea dictumst.
-	 		</p>
-	 		
-	 </div>
+	<?php include 'parts/navbar.php';?>
+
+</div>
+
+
+<div class="container">
+	<article id="article1" class="article card soft">
+	<div class="display-flex flex-alugn-center">
+		<div class="flex-stretch">
+			<h2>About</h2>
+		</div>
+		
 	</div>
+		<dive class="article-body">
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing, elit. Distinctio, magni aliquid, blanditiis molestias ratione reprehenderit ipsa placeat, voluptatibus excepturi deleniti iste quaerat laborum earum sapiente laboriosam quae sint, est veritatis tempora explicabo saepe natus! Amet quo, similique iure nobis nulla.</p>
+			<p>Nemo tempora alias quas nihil, doloribus, eos tempore voluptatibus aliquam impedit eligendi consectetur neque veniam, totam cum autem dolor in tenetur nisi saepe rem. Officia placeat nam ipsam explicabo corrupti quis ducimus praesentium molestiae, sapiente dignissimos quisquam numquam impedit voluptatum.</p>
+			<p>Obcaecati totam non ducimus similique exercitationem, eligendi, accusantium. Maxime sed delectus nobis, blanditiis facere qui harum pariatur veniam accusamus soluta, nihil suscipit voluptates obcaecati voluptatum. Aspernatur, ipsa quisquam velit ea ab quidem dolorem est, doloremque ipsum quis non quia ipsam?</p>
+		</dive>
+	</article>
+	
+</div>
+
+
+	
+
+</div>
+
+<?php include 'parts/footer.php';?>
 </body>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Product List</title>
-	<?php include "parts/meta.php"; ?>
-
-
-</head>
-<body>
-	<?php include "parts/navbar.php"; ?>
-
-	 <div class="container">
-	 	<div class="card soft">
-	 		<h2>About eNest</h2>
-	 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec diam nec ligula finibus auctor malesuada in ipsum. Aliquam cursus interdum fringilla. Suspendisse at sapien euismod mauris tristique commodo. Donec lorem felis, efficitur ultricies nulla sit amet, gravida hendrerit dui. Nullam porttitor tellus eu justo sollicitudin, nec mollis orci faucibus. Aliquam libero massa, fermentum at blandit at, blandit nec turpis. Nunc augue mi, laoreet egestas suscipit et, rutrum non purus. Quisque quis dui ante. Suspendisse ac pellentesque nisi. In hac habitasse platea dictumst.
-	 		</p>
-	 		
-	 </div>
-	</div>
-</body>
->>>>>>> 0d70d51e916c1d77fd00120dfad33dac2c6916e4
 </html>
--- He.Dan/product_list.php
+++ He.Dan/product_list.php
@@ -1,56 +1,83 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+
+include_once "lib/php/functions.php";
+include_once "parts/templates.php";
+
+
+
+?><!DOCTYPE html>
+<html>
 <head>
 	<meta charset="UTF-8">
-	<title>Product List</title>
-	<?php include "parts/meta.php"; ?>
+	
+<?php include "parts/meta.php"; ?>
+
+<script src="lib/js/functions.js"></script>
+<script src="js/templates.js"></script>
+<script src="js/product_list.js"></script>
+
 
 
 </head>
-<body>
-	<?php include "parts/navbar.php"; ?>
 
-	 <div class="container">
-	 	<div class="card soft">
-	 		<h2>Product List</h2>
-	 		<ul>
-	 			<li><a href="product_item.php?id=1">Product 1</a></li>
-	 			<li><a href="product_item.php?id=2">Product 2</a></li>
-	 			<li><a href="product_item.php?id=3">Product 3</a></li>
-	 			<li><a href="product_item.php?id=4">Product 4</a></li>
-	 		</ul>
-	 	</div>
-	 	
-        <h3>Product Grid</h3>
-        <div class="grid gap">
-            <div class="col-xs-12 col-md-4">
-                <figure class="figure product">
-                    <img src="https://images.unsplash.com/photo-1605648916361-9bc12ad6a569?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="">
-                    <figcaption>
-                        <div>Product Name</div>
-                        <div>3.99</div>
-                    </figcaption>
-                </figure>   
-            </div>
-            <div class="col-xs-12 col-md-4">
-                <figure class="figure product">
-                    <img src="https://images.unsplash.com/photo-1605648916361-9bc12ad6a569?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="">
-                    <figcaption>
-                        <div>Product Name</div>
-                        <div>3.99</div>
-                    </figcaption>
-                </figure>   
-            </div>
-            <div class="col-xs-12 col-md-4">
-                <figure class="figure product">
-                    <img src="https://images.unsplash.com/photo-1605648916361-9bc12ad6a569?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="">
-                    <figcaption>
-                        <div>Product Name</div>
-                        <div>3.99</div>
-                    </figcaption>
-                </figure>   
-            </div>
-        </div>
-	 </div>
+<body>
+	<div>
+		
+		
+
+		<?php include 'parts/navbar.php';?>
+
+	</div>
+
+
+	<div class="container">
+			<h2>Product List</h2>
+
+		<div class="form-control">
+			<form class="hotdog light" id="product-search">
+				<input type="search" placeholder="Search Products">
+			</form>
+		</div>
+		<div class="form-control ">
+			<div class="card soft">
+				<div class="display-flex flex-wrap">
+					<div class="flex-stretch display-flex">
+			
+						<div class="flex-none">
+							<button data-filter="color" data-value="acrylic" type="button">Acrylic</button>
+						</div>
+						<div class="flex-none">
+							<button data-filter="color" data-value="Gold" type="button">Gold</button>
+						</div>
+						<div class="flex-none">
+							<button data-filter="color" data-value="" type="button">All</button>
+						</div>
+					</div>
+		<div class="flex-none">
+
+	<div class="form-select">	
+					    	<select class="js-sort">
+								<option value="1">Newest</option>
+								<option value="2">Oldest</option>
+								<option value="3">Least Expensive</option>
+								<option value="4">Most Expensive</option>
+														
+								
+						</select>
+					</div>
+</div>
+</div>
+</div>
+</div>
+
+		<div class='productlist grid gap'></div>
+
+	</div>
+
+
+
+<?php include 'parts/footer.php';?>
+
 </body>
 </html>
+
